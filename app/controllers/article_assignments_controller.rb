b225708fbf11e406e3f06dc24f3a3e7f7fb0ad98
vrkolur <varunkolur17.com>
2024-05-14 22:52:22 +0530

    ArticleAssignment Service Done

--- app/controllers/article_assignments_controller.rb
+++ app/controllers/article_assignments_controller.rb
@@ -8,15 +8,13 @@ class ArticleAssignmentsController < ApplicationController
     end
 
     def create 
-        @author = User.find_by(id: params[:author_id])
-        msg = "Hey you are supposed to create a artile with title:#{params[:title]} and Category: #{Category.find_by(id: params[:category_id]).title} "
-        @notification = Message.create(sender:current_user, reciever:@author,msg: msg)
-        if @notification.save 
+        notification = Services::ArticleAssignmentService.new(params: params,current_user: current_user).create
+        if notification&.save 
             flash[:notice] = 'Article Assigned Successfully'
             redirect_to new_article_assignment_path(client_id: @client.sub_domain)
         else 
-            flash[:notice] = 'Error Occured please try again'
-            render :new
+            flash[:alert] = 'Error Occured please try again'
+            redirect_to new_article_assignment_path(client_id: @client.sub_domain)
         end
     end
 
@@ -25,7 +23,6 @@ class ArticleAssignmentsController < ApplicationController
 
     def check_admin 
         client_user = ClientUser.find_by(user: current_user) if current_user && current_user.role.title == 'ClientAdmin'
-        # @client = Client.find_by(sub_domain: params[:client_id])
         unless client_user&.client == @client 
             redirect_to articles_path(client_id: client_user&.client&.sub_domain)
         end
@@ -34,7 +31,7 @@ class ArticleAssignmentsController < ApplicationController
     def check_client
         @client =  Client.find_by(sub_domain: params[:client_id])
         unless @client
-          redirect_to root_path
+            redirect_to root_path
         end
     end
 end
