f292c50fce631bf1047cb3fa721c01f49525eb72
Varun Ravi Kolur <vrkolur@C02F96Y6MD6M.local>
2024-05-15 18:49:59 +0530

    last commit

--- lib/services/article_assignment_service.rb
+++ lib/services/article_assignment_service.rb
@@ -8,7 +8,7 @@ module Services
         def create
             if check_params
                 @author = User.find_by(id: @params[:author_id])
-                msg = "Hey you are supposed to create a artile with title:#{params[:title]} and Category: #{Category.find_by(id: params[:category_id]).title} "
+                msg = "Hey you are supposed to create a artile with title:#{@params[:title]} and Category: #{Category.find_by(id: @params[:category_id]).title} "
                 @notification = Message.create(sender: @current_user, reciever: @author,msg: msg)
             else 
                 return nil 
@@ -17,9 +17,9 @@ module Services
 
         def check_params
             unless @params[:author_id].nil? && @params[:title].nil? && @params[:category_id].nil? && @current_user.nil? 
-                return false
+                return true
             end
-            return true
+            return false
         end
     end
 end
